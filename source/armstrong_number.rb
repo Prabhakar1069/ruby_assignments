9cd5cd8e1886dcb4610e8df0b84a0d4d8259151c
Prabhakar Kumar <prabhakar1069@gmail.com>
2021-01-11 16:59:39 +0530

    armstrong_number.rb was done

--- source/armstrong_number.rb
+++ source/armstrong_number.rb
@@ -1,28 +1,30 @@
 class ArmstrongNumber
-	def initialize(num)
+  def initialize(num)
 		@num = num
 
+  end	
+  def count_digit
+	digit = @num
+	count = 0
+	while (digit!=0)
+		count = count+1
+		digit = digit/10
 	end	
-	def count_digit
-		digit = @num
-		count = 0
-		while (digit!=0)
-			count = count+1
-			digit = digit/10
-		end	
-		return count
+	return count
 	end	
-	def is_armstrong
-		sum = 0
-		x = @num
-		count = count_digit
-		while (x!=0)
-			rem = x%10
-			sum = sum+(rem**count)
-			x = x/10
-		end	
-		return true if sum==@num
+  def is_armstrong
+	sum = 0
+	x = @num
+	count = count_digit
+	while (x!=0)
+		rem = x%10
+		sum = sum+(rem**count)
+		x = x/10
+	end	
+	return true if sum==@num && @num!=0
 
-		return false
-	end	
+	return false
+  end	
 end	
+
+
