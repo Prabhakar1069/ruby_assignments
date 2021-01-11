2ff9ea584bc76b21d8539374003f5df7fbc643c8
Prabhakar Kumar <prabhakar1069@gmail.com>
2021-01-11 17:02:05 +0530

    freq_count.rb was done

--- source/freq_count.rb
+++ source/freq_count.rb
@@ -1,17 +1,17 @@
 class FreqCount
-	def initialize(string)
-		@string  = string
-	end
-	def frequency
-		mp = Hash.new(0)
+  def initialize(string)
+	@string  = string
+  end
+  def frequency
+	mp = Hash.new(0)
 
-		for char in @string.chars.to_a
-			mp[char]=mp[char]+1
-		end	
-		return mp
+	for char in @string.chars.to_a
+		mp[char]=mp[char]+1
 	end	
+	return mp
+  end	
 end
 
-# m = FreqCount.new("a1ab2b").frequency
+
 
 
